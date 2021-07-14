f0fbde85f45a47ad24206af944c1b9fa36849b59
Mbulelo Pani <mbulelopani09@gmail.com>
2021-07-15 01:00:26 +0200

    prepare to deploy

--- src/App.vue
+++ src/App.vue
@@ -10,18 +10,14 @@
 
 <script>
 
-import AddBlog from './components/addBlog.vue';
 import homeView from './components/home.vue'
-import listMovies from './components/listOfMovies.vue'
 import header from './components/header.vue';
 import navButtons from './components/navButtons.vue'
 
 
 export default {
   components: {
-    'add-blog': AddBlog,
     'home-view': homeView,
-    'list-movies': listMovies,
     'app-header': header ,
     'nav-buttons': navButtons 
   },
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,7 @@
 import home from './components/home.vue'
-import listMovies from './components/listOfMovies.vue'
 import movieItem from './components/movieItem.vue'
 export default [
     {path: '/', component: home},
-    {path: '/list', component: listMovies},
     {path: '/movie/:id', component: movieItem}
 
 ]
